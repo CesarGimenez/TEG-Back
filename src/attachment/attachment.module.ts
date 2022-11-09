2bb16e058b840b1a3f8433f55685073b561dc416
Cesar Gimenez <cesargimenez760@gmail.com>
2022-11-09 08:42:21 -0400

    modules attachment and diagnostic

--- src/attachment/attachment.module.ts
+++ src/attachment/attachment.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AttachmentController } from './attachment.controller';
+import { AttachmentService } from './attachment.service';
 
-@Module({})
+@Module({
+  controllers: [AttachmentController],
+  providers: [AttachmentService]
+})
 export class AttachmentModule {}
